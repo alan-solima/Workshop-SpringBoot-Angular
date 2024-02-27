95a6c8255520fe4f4c23710e13c58e79371e0d76
Alan <contact.alansoareslima@gmail.com>
2024-02-26 22:55:00 -0300

    Entity config

---
+++ src/main/java/br/com/projeto/api/Model/Customer.java
@@ -0,0 +1,27 @@
+package br.com.projeto.api.Model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long code;
+
+    private String name;
+
+    private int age;
+
+    private String city;
+}
--- src/main/java/br/com/projeto/api/Model/Model.java
+++
@@ -1,5 +0,0 @@
-package br.com.projeto.api.Model;
-
-public class Model {
-    
-}
