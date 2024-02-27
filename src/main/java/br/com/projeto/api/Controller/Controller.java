979cda4b6abc011139ecf4147fad72246e4e086b
Alan <contact.alansoareslima@gmail.com>
2024-02-26 23:19:14 -0300

    Register route

--- src/main/java/br/com/projeto/api/Controller/Controller.java
+++ src/main/java/br/com/projeto/api/Controller/Controller.java
@@ -1,15 +1,24 @@
 package br.com.projeto.api.Controller;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.Model.Customer;
+import br.com.projeto.api.Repository.Repository;
 
 @RestController
 public class Controller {
     
     @Autowired
     private Repository action;
+
+    @PostMapping("/")
+    public Customer register(@RequestBody Customer c){
+        return action.save(c);
+    }
 
     @GetMapping("/")
     public String teste() {
