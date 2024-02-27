8d0fe6d09d595110f16f61a978cef233a7ebcdac
Alan <contact.alansoareslima@gmail.com>
2024-02-26 23:11:08 -0300

    Annotation @Autowired

--- src/main/java/br/com/projeto/api/Controller/Controller.java
+++ src/main/java/br/com/projeto/api/Controller/Controller.java
@@ -1,11 +1,16 @@
 package br.com.projeto.api.Controller;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     
+    @Autowired
+    private Repository action;
+
     @GetMapping("/")
     public String teste() {
         return "Hello world!";
