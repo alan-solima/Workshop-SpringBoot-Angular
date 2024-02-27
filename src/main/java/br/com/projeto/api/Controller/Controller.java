03842e9185c410f54ef9521b1da0131bb719236d
Alan <contact.alansoareslima@gmail.com>
2024-02-26 22:46:49 -0300

    Controller config

--- src/main/java/br/com/projeto/api/Controller/Controller.java
+++ src/main/java/br/com/projeto/api/Controller/Controller.java
@@ -1,5 +1,13 @@
 package br.com.projeto.api.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
     
+    @GetMapping("/")
+    public String teste() {
+        return "Hello world!";
+    }
 }
