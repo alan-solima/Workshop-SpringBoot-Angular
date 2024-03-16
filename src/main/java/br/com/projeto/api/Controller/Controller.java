5a8038480d241dcdc8883edea03e048b80c59353
Alan <contact.alansoareslima@gmail.com>
2024-03-15 22:29:10 -0300

    Edit route

--- src/main/java/br/com/projeto/api/Controller/Controller.java
+++ src/main/java/br/com/projeto/api/Controller/Controller.java
@@ -5,6 +5,7 @@ import br.com.projeto.api.Repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class Controller {
     @GetMapping("/")
     public Iterable<Customer> select() {
         return action.findAll();
+    }
+
+    @PutMapping("/")
+    public Customer edit(@RequestBody Customer c) {
+        return action.save(c);
     }
 }
